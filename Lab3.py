956791dcb5837032943062f97d98ce01b4beb83a
Agayefh <uagayevr14@gmail.com>
2025-03-13 09:10:04 +0400

    Create Lab3.py

---
+++ Lab3.py
@@ -0,0 +1,17 @@
+import math
+
+x = 0.2
+a = 1
+epsilon = 0.01
+
+ax = a * x
+term = math.log(ax)
+P = 1 + term
+n = 2
+
+while abs(term / n) >= epsilon:
+    term = (math.log(ax) ** n) / n
+    P += term
+    n += 1
+
+print(f"P ≈ {P:.4f}")
